63a3965f8e013805338ad01882e6e87e062d450b
PruthuviHasanjana <2017e009@eng.jfn.ac.lk>
2020-05-26 08:37:45 +0530

    Created Routes Responses in Express

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/users', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all user' });
+});
+
+app.get('/api/v1/users/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `get user ${req.params.id}` });
+});
+
+app.post('/api/v1/users', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new' });
+});
+
+app.put('/api/v1/users/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Update user ${req.params.id}` });
+});
+
+app.delete('/api/v1/users/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
