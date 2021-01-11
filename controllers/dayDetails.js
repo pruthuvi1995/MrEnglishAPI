9d1676f0f30f837493d786d6166d62dfd4bb0fe6
PruthuviHasanjana <2017e009@eng.jfn.ac.lk>
2021-01-11 22:09:16 +0530

    getOtp was updated 8

--- controllers/dayDetails.js
+++ controllers/dayDetails.js
@@ -153,9 +153,8 @@ let response;
   axios
   .post('https://api.dialog.lk/subscription/otp/request',details,{ headers: {         'Content-Type': 'application/json'     } } )
   .then(res => {
-    
+    console.log(res);
       response=res;
-   
   })
   .catch(error => {
     console.error(error);
