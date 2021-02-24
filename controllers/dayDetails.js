5c6ba0328bc097238818a44cc2667804076b161d
PruthuviHasanjana <2017e009@eng.jfn.ac.lk>
2021-02-24 09:17:14 +0530

    phone number subString was updated

--- controllers/dayDetails.js
+++ controllers/dayDetails.js
@@ -160,7 +160,7 @@ exports.verifyOtp = asyncHandler(async (req, res, next) => {
 }
 
 
-  axios
+ await axios
   .post('https://api.dialog.lk/subscription/otp/verify', data, { headers: { 'Content-Type': 'application/json'} })
   .then(res => {
     console.log(res['data']);
@@ -193,7 +193,7 @@ exports.getSubs = asyncHandler(async (req, res, next) => {
 }
 
 
-  axios
+  await axios
   .post('https://api.dialog.lk/subscription/getStatus', data, { headers: { 'Content-Type': 'application/json'} })
   .then(res => {
     console.log(res['data']);
