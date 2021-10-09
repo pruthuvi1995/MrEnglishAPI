1b96beaf1c75ad965c6b4bebfd8c7f0ee400aa46
Pruthuvi1995 <pruthuvi1995@gmail.com>
2021-10-09 21:53:00 +0530

    add year details was updated

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    // useUnifiedTopology: true,
   });
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
 };
