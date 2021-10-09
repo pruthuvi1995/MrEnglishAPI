d97cfc067ae7c78315b8635433b104456b6da482
Pruthuvi1995 <pruthuvi1995@gmail.com>
2021-10-09 23:22:34 +0530

    add year details was updated

--- controllers/yearDetails.js
+++ controllers/yearDetails.js
@@ -58,7 +58,7 @@ exports.addYearDetails = asyncHandler(async (req, res, next) => {
   req.body.user = req.params.userId;
   req.body.year = req.params.yearId;
 
-  const yearDetails = await YearDetail.find( {$and: [{user: req.body.user}, {day: req.body.year }]});
+  const yearDetails = await YearDetail.find( {$and: [{user: req.body.user}, {year: req.body.year }]});
 
   if (yearDetails.length != 0) {
     return res.status(200).json({
